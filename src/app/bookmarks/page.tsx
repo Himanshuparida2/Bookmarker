e169761da76291f8864dd48c7265783884e0bd5c
Himanshu Sekhar Parida <himanshu.parida16@gmail.com>
2026-02-13 20:05:24 +0530

    DynamoDb Added

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,7 +1,8 @@
 "use client";
+import dotenv from 'dotenv';
 import React, { useEffect, useRef } from 'react'
 import { useState } from 'react';
-
+dotenv.config();
 function bookmarks() {
   const bookmarkref = useRef(null);
   const [response, setResponse] = useState([]);
@@ -20,10 +21,22 @@ function bookmarks() {
     console.log("Bookmarks updated:", bookmark);
   };
 
+  const fetchItm = () => {
+    console.log(process.env.NEXT_PUBLIC_URL);
+    fetch(`${process.env.NEXT_PUBLIC_URL}?email=${encodeURIComponent("test@test.com")}`,
+      { method: 'GET',}
+      ).then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    }).catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+  }
   return (
     <div>
-      <input type="text" name='bookmark-input' ref={bookmarkref} className='border-1'/>
+      <input type="text" name='bookmark-input' ref={bookmarkref} className='border'/>
       <input type="button" value="add bookmark" className='border-2 hover:cursor-pointer' onClick={() => updateBookmarks(bookmarkref.current.value)} />
+      <input type="button" value="fetch" className='border' onClick={()=>{fetchItm()}} />
       <p>{response}</p>
     </div>
   )
